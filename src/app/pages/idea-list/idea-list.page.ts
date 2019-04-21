57e332691d5705e8e817584f082201c7efe856b1
Rajab Mkomwa <rajeyw@gmail.com>
2019-04-22 00:29:20 +0300

    fix: resolve issues to make travis work

--- src/app/pages/idea-list/idea-list.page.ts
+++ src/app/pages/idea-list/idea-list.page.ts
@@ -1,18 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { IdeaService, Idea } from 'src/app/services/idea.service';
 import { Observable } from 'rxjs';
- 
+
 @Component({
   selector: 'app-idea-list',
   templateUrl: './idea-list.page.html',
-  styleUrls: ['./idea-list.page.scss'],
+  styleUrls: ['./idea-list.page.scss']
 })
 export class IdeaListPage implements OnInit {
- 
-  private ideas: Observable<Idea[]>;
- 
-  constructor(private ideaService: IdeaService) { }
- 
+  ideas: Observable<Idea[]>;
+
+  constructor(private ideaService: IdeaService) {}
+
   ngOnInit() {
     this.ideas = this.ideaService.getIdeas();
   }
