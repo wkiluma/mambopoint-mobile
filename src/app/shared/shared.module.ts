3e725e6583bdcc6882b63e034bbdc4e1749fddf5
Rajab Mkomwa <rajeyw@gmail.com>
2019-04-23 00:15:00 +0300

    refactor: remove testing pages and services

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { components } from './components';
+import { directives } from './directives';
+import { pipes } from './pipes';
+import { services } from './services';
+
+@NgModule({
+  declarations: [...components, ...directives, ...pipes],
+  providers: [...services],
+  imports: [CommonModule]
+})
+export class SharedModule {}
